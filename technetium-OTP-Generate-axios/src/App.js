cfcd36984c5cbbff08fb717c4776ed272a42d0de
Purushottam mishra <78618623+Purushottam67@users.noreply.github.com>
2023-05-05 07:22:40 +0530

    Add files via upload

---
+++ technetium-OTP-Generate-axios/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import OtpGenerate from './components/atoms/OtpGenerate';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <OtpGenerate/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
